e75252d605e72dce46e632e602cfe245a5393da1
RAITILAK <raitilak1212@gmail.com>
2025-07-02 02:11:44 +0530

    deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -77,9 +77,9 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
   });
 }
 

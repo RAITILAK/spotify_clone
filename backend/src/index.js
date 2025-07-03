46b3f0be3310a7e5a4231e5ec9aedcf5c1ad5da2
RAITILAK <raitilak1212@gmail.com>
2025-07-04 00:58:11 +0530

    Refactor environment variable loading and resolve __dirname in ESM

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,17 @@
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolve __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// import dotenv from "dotenv";
+// import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
-import path from "path";
+// import path from "path";
 import cors from "cors";
 import { createServer } from "http";
 
@@ -23,12 +32,13 @@ process.on("unhandledRejection", (err) => {
   console.error("❌ Unhandled Promise Rejection:", err);
 });
 
-dotenv.config();
+dotenv.config({
+  path: path.resolve(__dirname, "../..", ".env"),
+});
 
 console.log("✅ Loaded environment variables");
 
 const app = express();
-const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 console.log("🚀 PORT:", PORT);

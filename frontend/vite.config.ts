7c135568eb2498b42ffc4abf3ce6c5acb6547057
RAITILAK <raitilak1212@gmail.com>
2025-07-03 15:26:15 +0530

    fixed!

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-// Fix __dirname for ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"), // same effect as path.resolve
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {

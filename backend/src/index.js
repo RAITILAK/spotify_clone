fa66d930b8e0eff92910a83d80d9e9152a6f0638
RAITILAK <raitilak1212@gmail.com>
2025-07-04 01:44:47 +0530

    tsconfig.json and index.js changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 
 // Resolve __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // import dotenv from "dotenv";
 // import express from "express";
@@ -32,9 +32,11 @@ process.on("unhandledRejection", (err) => {
   console.error("❌ Unhandled Promise Rejection:", err);
 });
 
-dotenv.config({
-  path: path.resolve(__dirname, "../..", ".env"),
-});
+// dotenv.config({
+//   path: path.resolve(__dirname, "../..", ".env"),
+// });
+const __dirname = path.resolve();
+dotenv.config();
 
 console.log("✅ Loaded environment variables");
 
